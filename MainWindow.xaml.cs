9d54a9ff26484b80b53d11b5ef7daac1ed13395c
pinkypie <b4jgiel@gmail.com>
2026-07-27 16:01:53 +0200

    Remove Discord client ID and update GitHub URL

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,8 +13,8 @@ namespace VxPresence
     public partial class MainWindow : Window
     {
         // ⚠️ PAMIĘTAJ O USUNIĘCIU SWOICH DANYCH PRZED COMMITEM NA GITHUB!
-        private const string DISCORD_CLIENT_ID = "1531223190637117580";
-        private const string GITHUB_URL = "https://github.com/444vx"; 
+        private const string DISCORD_CLIENT_ID = "";
+        private const string GITHUB_URL = "https://github.com/444vx/vxpresence"; 
 
         private CpuMonitor? _cpuMonitor;
         private RamMonitor? _ramMonitor;
